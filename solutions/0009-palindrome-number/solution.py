f242b7668919cd9f07c672c29c9c4386c7c13a4d
Aadhitya Balaji <145163888+aadhibalaji@users.noreply.github.com>
2026-06-05 20:48:58 +0000

    Sync LeetCode submission Runtime - 12 ms (49.01%), Memory - 12.3 MB (90.22%)

---
+++ solutions/0009-palindrome-number/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        s = str(x)
+
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+            if (s[l] != s[r]):
+                return False
+
+            l += 1
+            r -= 1
+
+        return True        
+        
