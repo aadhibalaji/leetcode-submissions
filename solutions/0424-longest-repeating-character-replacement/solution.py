f6ed2f4cbe58bfba4bbe34159eaddec83a447793
Aadhitya Balaji <145163888+aadhibalaji@users.noreply.github.com>
2026-08-10 21:07:54 +0000

    Sync LeetCode submission Runtime - 131 ms (80.60%), Memory - 15.4 MB (62.04%)

---
+++ solutions/0424-longest-repeating-character-replacement/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        
+        table = {}
+        res = 0
+        left = 0
+        maxFreq = 0
+        for right in range(len(s)):
+            table[s[right]] = table.get(s[right], 0) + 1
+            maxFreq = max(maxFreq, table[s[right]])
+
+            while (right - left + 1) - maxFreq > k:
+
+                table[s[left]] -= 1
+                left += 1
+
+            res = max(res, right - left + 1)
+                
+        return res
+            
