75275a5afe7609175c430995745b58d898d4d740
Aadhitya Balaji <145163888+aadhibalaji@users.noreply.github.com>
2026-08-13 22:04:09 +0000

    Sync LeetCode submission Runtime - 3 ms (99.11%), Memory - 14.6 MB (41.07%)

--- solutions/0253-meeting-rooms-ii/solution.py
+++ solutions/0253-meeting-rooms-ii/solution.py
@@ -5,22 +5,17 @@ class Solution(object):
         :rtype: int
         """
         
-
         intervals.sort(key = lambda i : i[0])
 
         heap = []
-
         heapq.heappush(heap, intervals[0][1])
-        
-        
-        for start, end in intervals[1:]:
 
-            if heap[0] <= start:
+        for start, end in intervals[1:]:
+            if start < heap[0]:
+                heapq.heappush(heap, end)
+            else:
                 heapq.heappop(heap)
                 heapq.heappush(heap, end)
-                continue
 
-            heapq.heappush(heap, end)
-
+        print(heap)
         return len(heap)
-
